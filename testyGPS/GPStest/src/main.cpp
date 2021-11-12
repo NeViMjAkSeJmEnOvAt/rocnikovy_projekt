0d41625cabe0018a2428075c0b85c100e7b40a12
nevimjaksejmenovat <mikurecka@gmail.com>
2021-11-12 08:55:18 +0100

    gps funguje

--- testyGPS/GPStest/src/main.cpp
+++ testyGPS/GPStest/src/main.cpp
@@ -5,26 +5,32 @@ Adafruit_GPS GPS(&GPSSerial);
 
 void setup()
 {
-  GPSSerial.begin(9600, SERIAL_8N1,16,17);
+  GPSSerial.begin(9600);
   Serial.begin(115200);
-  delay(1000);
-  Serial.println("Adafruit GPS logging data dump!");
-  // 9600 NMEA is the default baud rate for MTK - some use 4800
   GPS.begin(9600);
-  delay(1000);
   Serial.println("----------------------------------------------------");
-
-  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_ALLDATA);
 }
-
-
-uint32_t updateTime = 1000;
+uint32_t updateTime = 100;
 
 void loop(){
   GPS.read();
   if(GPS.newNMEAreceived()){
       GPS.parse(GPS.lastNMEA());
+      Serial.print("Den: ");
       Serial.println(GPS.day);
+      Serial.print("Hodina: ");
+      Serial.println(int(GPS.hour+1));
+      Serial.print("Pocet Satelitu: ");
+      Serial.println(GPS.satellites);
+      Serial.print("Latitude: ");
+      Serial.print(GPS.latitude);
+      Serial.println(GPS.lat);
+      Serial.print("Lognitude: ");
+      Serial.print(GPS.longitude);
+      Serial.println(GPS.lon);
+
+      
+      
   }
   
   delay(100);
